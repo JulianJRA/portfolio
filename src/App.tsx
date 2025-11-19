b9b1e1b073de7dc37b0ed89b74d6ae2b36666992
JulianJRA <julianjra5@gmail.com>
2025-11-19 09:42:31 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <div className="bg-red-500 text-white p-10 rounded-xl text-center text-3xl font-bold">
+        Tailwind Funciona 🎉
+      </div>
+    </div>
+  );
+};
+
+export default App;
